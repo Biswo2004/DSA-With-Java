ee4b285e7a30fba734399789d40e6d840b17074c
Biswojit Bal <118650829+Biswo2004@users.noreply.github.com>
2024-05-20 11:32:19 +0530

    Add files via upload
    ->Stack Data Structure
    This is based on the Push, Pop and Peek Functionality of a Stack using Array, ArrayList, LinkedList.

---
+++ Stack/StackDemo1.java
@@ -0,0 +1,82 @@
+package Stack;
+
+import java.util.Scanner;
+
+public class StackDemo1 {
+    public static final int MAX = 10;
+
+    public static int push(int S[], int top, int x) {
+        if (isFull(top)) {
+            System.out.println("Stack is full");
+            return top;
+        }
+        top++;
+        S[top] = x;
+        System.out.println("Pushed Element is:  "+x);
+        return top;
+    }
+
+    public static int pop(int S[], int top) {
+        if (isEmpty(top)) {
+            System.out.println("Stack is empty");
+            return top;
+        }
+        int data = S[top];
+        top--;
+        System.out.println("Popped element is: " + data);
+        return top;
+    }
+
+    public static void display(int S[], int top) {
+        if (isEmpty(top)) {
+            System.out.println("Stack is empty");
+            return;
+        }
+        System.out.println("Elements in the stack are:");
+        for (int i = 0; i <= top; i++) {
+            System.out.println(S[i]);
+        }
+    }
+
+    public static boolean isEmpty(int top) {
+        return (top == -1);
+    }
+
+    public static boolean isFull(int top) {
+        return (top == MAX - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int stack[] = new int[MAX];
+        int top = -1;
+        while (true) {
+            System.out.println("****MENU****");
+            System.out.println("0: Exit");
+            System.out.println("1: Push");
+            System.out.println("2: Pop");
+            System.out.println("3: Display");
+            System.out.println("Enter your choice");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 0:
+                    System.exit(0);
+                    System.out.println("Exiting....");
+                case 1:
+                    System.out.println("Enter the element to be pushed");
+                    int x = sc.nextInt();
+                    top = push(stack, top, x);
+                    break;
+                case 2:
+                    top = pop(stack, top);
+                    break;
+                case 3:
+                    display(stack, top);
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }
+    }
+}
+
